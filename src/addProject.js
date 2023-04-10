f7d90647ed7288f8e6104ebbb5a21343107647df
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-04-10 17:52:27 -0500

    Display added project title to the sidebar

--- src/addProject.js
+++ src/addProject.js
@@ -1,22 +1,29 @@
 const addProject = (function () {
+
+    const addProject = document.querySelector('.add-project')
+    const form = document.querySelector('form')
+    const title = document.querySelector('#title')
+    const addButton = document.querySelector('.add')
+    const cancelButton = document.querySelector('.cancel')
    
     const displayForm = function () {
-        const addProject = document.querySelector('.add-project')
-        const title = document.querySelector('#title')
-        const addButton = document.querySelector('.add')
-        const cancelButton = document.querySelector('.cancel')
+        
         addProject.style.display = 'flex'
 
         addButton.addEventListener('click', (event) => {
-            if (title.value != '' ) {
+            if (title.value != '') {
                 event.preventDefault()
+                displayProject()
                 addProject.style.display = 'none'
+                console.log(title.value)
+                form.reset()
             }
         })
 
         cancelButton.addEventListener('click', (event) => {
             event.preventDefault();
             addProject.style.display = 'none'
+            form.reset()
         })
 
         addProject.addEventListener('click', (event) => {
@@ -25,6 +32,20 @@ const addProject = (function () {
             }
         })
     }
+
+    const displayProject = function () {
+        const projectsList = document.querySelector('.projects-list')
+
+        const projectDiv = document.createElement('div')
+        projectDiv.classList.add('.project-display')
+        projectsList.appendChild(projectDiv)
+
+        const projectTitleDiv = document.createElement('div')
+        projectDiv.textContent = title.value
+        projectDiv.appendChild(projectTitleDiv)
+
+    }
+
 
     return {
         displayForm
