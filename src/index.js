1144d4adf2d43b9cdb68e22c981085335441a694
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-04-11 21:08:21 -0500

    Add functionality to find the index of a project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import addProject from './addProject'
+import addProjectList from './addProject'
+import tasks from './tasks'
 
 const newProject = document.querySelector('.project-button')
+const projectButton = document.querySelector('.projects-lists')
 
-newProject.addEventListener('click', addProject.displayForm)
+newProject.addEventListener('click', addProjectList.displayForm)
+
+projectButton.addEventListener('click', (project) => {
+    if (project.target.classList.contains('project')) {
+       
+        let targetProject = project.target.parentNode.parentNode
+        let index = Array.from(targetProject.children).indexOf(project.target.parentNode)
+        
+        tasks(project.target,index)
+    }
+})
---
+++ src/tasks.js
@@ -0,0 +1,9 @@
+import addProjectList from './addProject'
+
+const tasks = function (project,index) {
+    
+    console.log(addProjectList.myProjectArray[index])
+    console.log(index)
+}
+
+export default tasks
