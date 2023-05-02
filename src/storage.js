692e0d7f192feed69c1813cb04b6ee2e86cc0be9
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-05-02 18:23:21 -0500

    Add default projects to display
    Display default projects for users to view
    
    If the user deletes all the projects they added and all the default
    projects, default projects still shows up after refresh

--- src/storage.js
+++ src/storage.js
@@ -5,16 +5,40 @@ const storage = (function () {
 
     function populateData() {
         if (localStorage.getItem('project')) {
-
+           
             let storageArray = JSON.parse(localStorage.getItem('project'))
 
             for (let i = 0; i < storageArray.length; i++){
                 addProjectList.myProjectArray.push(storageArray[i])
-
-                console.log(storageArray[i])
             }
 
             dom.createProjectDiv()
+
+        } else if (!localStorage.getItem('project')) {
+            
+            console.log('test')
+            let defaultProjects = [{title: 'Gym',
+                                    description: 'List of workouts',
+                                    date: '',
+                                    priority: 'Medium',
+                                    tasks: [{ taskName: 'Deadlifts', isCompleted: false },        
+                                            { taskName: 'Squat', isCompleted: false },
+                                            { taskName: 'Bench Press', isCompleted: false }]
+                                    },
+                                    
+                                    {title: 'Class',
+                                    description: 'List of Home works',
+                                    date: '2023-05-19',
+                                    priority: 'High',
+                                    tasks: [{taskName: 'Physics Assignment', isCompleted: true}]
+                                    }]
+            
+            for (let i = 0; i < defaultProjects.length; i++){
+                addProjectList.myProjectArray.push(defaultProjects[i])
+            }
+
+            dom.createProjectDiv()
+            storeData()
         }
     }
 
@@ -24,7 +48,9 @@ const storage = (function () {
 
         localStorage.setItem('project', JSON.stringify(storageArray))
 
-        console.log(JSON.parse(localStorage.getItem('project')))
+        if (storageArray.length == 0) {
+           localStorage.clear()
+       }
     }
 
     return {
